8b69350664aba50117154ce0ccc7c4961a5e12f5
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2024-02-08 23:40:21 -0500

    Create sillhouettes

--- convert.py
+++ convert.py
@@ -1,45 +1,45 @@
 from PIL import Image
 
-def png_to_svg(png_file, svg_file):
-    try:
-        # Open the PNG file
-        img = Image.open(png_file)
-        img = img.convert("RGBA")  # Ensure it's in RGBA format
-        width, height = img.size
+def png_to_silhouette(png_file, svg_file):
+    # Open the PNG file
+    img = Image.open(png_file)
+    img = img.convert("RGBA")  # Ensure it's in RGBA format for transparency support
+    width, height = img.size
 
-        # SVG header
-        svg_data = f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">\n'
+    # SVG header
+    svg_data = f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">\n'
 
-        # Process the image pixels
-        for y in range(height):
-            x = 0
-            while x < width:
-                r, g, b, a = img.getpixel((x, y))
+    # Process the image pixels
+    for y in range(height):
+        x = 0
+        while x < width:
+            r, g, b, a = img.getpixel((x, y))
 
-                # If the pixel is not transparent, start creating a rectangle
-                if a != 0:
-                    start_x = x
-                    # Find the length of the segment with the same color
-                    while x < width and img.getpixel((x, y)) == (r, g, b, a):
-                        x += 1
-                    length = x - start_x
-
-                    # Add a rectangle to the SVG for this segment
-                    svg_data += f'<rect x="{start_x}" y="{y}" width="{length}" height="1" fill="rgb({r},{g},{b})" fill-opacity="{a/255}"/>\n'
-                else:
+            # If the pixel is not transparent, start a new rectangle
+            if a != 0:
+                start_x = x
+                # Extend the rectangle width as long as the next pixel is also not transparent
+                while x < width and img.getpixel((x, y))[3] != 0:
                     x += 1
+                length = x - start_x
 
-        # SVG footer
-        svg_data += '</svg>'
+                # Add a rectangle for this segment
+                svg_data += f'<rect x="{start_x}" y="{y}" width="{length}" height="1" fill="black"/>\n'
+            else:
+                x += 1
 
-        # Write the SVG data to a file
-        with open(svg_file, 'w') as f:
-            f.write(svg_data)
+    # SVG footer
+    svg_data += '</svg>'
 
-        print(f"SVG file '{svg_file}' successfully created.")
+    # Write the SVG data to a file
+    with open(svg_file, 'w') as f:
+        f.write(svg_data)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    print(f"SVG silhouette file '{svg_file}' successfully created.")
 
 # Usage example:
-png_to_svg('input.png', 'output.svg')
+png_to_silhouette('./DexHouse.png', './Silhouettes/Dex.svg')
+png_to_silhouette('./Higsby\'s.png', './Silhouettes/Higsby.svg')
+png_to_silhouette('./HikariHome.png', './Silhouettes/Lan.svg')
+png_to_silhouette('./MaylsHome.png', './Silhouettes/Mayl.svg')
+png_to_silhouette('./YaisHome.png', './Silhouettes/Yai.svg')
